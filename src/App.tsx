676cc5a6f085ac10fa18da345b943f19b912bf0f
Sofia Bargues <barguessofia@gmail.com>
2024-02-19 14:50:37 +0000

    first

--- src/App.tsx
+++ src/App.tsx
@@ -16,27 +16,25 @@ function App() {
   return (
     <>
       <div
-        className={`${styles.paddingX} ${styles.flexCenter} bg-primary w-full overflow-hidden sticky top-0 z-[9]`}
+        className={`${styles.paddingX} ${styles.flexCenter} bg-blue-950 w-full overflow-hidden sticky top-0 z-[9]`}
       >
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
       <div className="bg-primary w-full overflow-hidden">
-        <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`bg-slate-800 ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
             <Hero />
           </div>
         </div>
 
-        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`bg-slate-800 ${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
-            <Stats />
-            <Business />
-            <Billing />
             <CardDeal />
-            <Testimonials />
+            <Billing />
             <Clients />
+            <Testimonials />
             <CTA />
             <Footer />
           </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,22 +8,26 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
-
+    <nav className="w-full flex py-0 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" />
+      <p
+        className={`font-poppins cursor-pointer text-[36px] text-dimWhite font-regular`}
+      >
+        Addiscombe Rifle Club
+      </p>
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`group font-poppins cursor-pointer text-[16px] ${
-              active === nav.title ? "text-secondary font-semibold" : "text-dimWhite font-regular"
+              active === nav.title
+                ? "text-secondary font-semibold"
+                : "text-dimWhite font-regular"
             } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
             onClick={() => setActive(nav.title)}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
-            <div
-              class="h-0.5 bg-secondary scale-x-0 group-hover:scale-100 transition-transform origin-left rounded-full duration-300 ease-out"
-            />
+            <div class="h-0.5 bg-secondary scale-x-0 group-hover:scale-100 transition-transform origin-left rounded-full duration-300 ease-out" />
           </li>
         ))}
       </ul>
