d47da4e25cf7fc495aa58290f923b5582ee80aa0
Sofia Bargues <barguessofia@gmail.com>
2024-02-28 19:21:09 +0000

    gf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="navbar w-full bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Akbennett</a>
+        <a className="btn btn-ghost text-xl">A.K. Bennett</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
