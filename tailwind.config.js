54e659d80a893014cbaab3923c906f387754ceb6
Sofia Bargues <barguessofia@gmail.com>
2024-03-01 07:57:52 +0000

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,10 @@
 
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  mode: "jit",
   theme: {
     extend: {
-      colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
-      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
-      },
-      boxShadow: {
-        woke: "0px 9.9px 21.6px rgba(9, 151, 124, 0.41)",
       },
     },
     screens: {
