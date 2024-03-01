a65545497852ac1d37c400a110db0f76b1f1ebcb
Sofia Bargues <barguessofia@gmail.com>
2024-03-01 07:44:20 +0000

    Delet componets

--- src/App.tsx
+++ src/App.tsx
@@ -31,13 +31,6 @@ function App() {
 
         <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
-            <Stats />
-            <Business />
-            <Billing />
-            <CardDeal />
-            <Testimonials />
-            <Clients />
-            <CTA />
             <Footer />
           </div>
         </div>
