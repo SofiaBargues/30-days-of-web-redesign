adb40e1505a0d0568345a174fe35d48991c256a5
Sofia Bargues <barguessofia@gmail.com>
2024-03-01 00:35:09 +0000

    sedf

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,19 +3,23 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const About = () => (
-  <section id="AboutUs">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn m-1">
-        Click
-      </div>
-      <div
-        tabIndex={0}
-        className="dropdown-content z-[1] card card-compact w-64 p-10 shadow bg-primary text-primary-content"
-      >
-        <div className="card-body">
-          <h3 className="card-title">Card title!</h3>
-          <p>you can use any element as a dropdown.</p>
-        </div>
+  <section id="AboutUs" className="my-40 mx-96">
+    <div className="card w-96 bg-base-100 shadow-2xl image-full">
+      <figure>
+        <img
+          src="https://happyjackssoftplay.co.uk/wp-content/uploads/2020/05/monkey.swing_.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">About Us</h2>
+        <p>
+          With over 10 years experience and more than 30 years qualified as a
+          gymnastics coach, we are confident that we can not only provide you
+          with an amazing safe environment for your child to play but also work
+          with you to provide a great play space to entertain children at home,
+          your event or party venue.
+        </p>
       </div>
     </div>
   </section>
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -9,7 +9,7 @@ import { grid } from "../assets";
 
 const Stats = () => (
   <section className="" id="HappyjacksGallery">
-    <div className="h-screen my-10 card text-center bg-base-100 shadow-xl">
+    <div className="mx-10 h-screen my-10 card text-center bg-base-300 shadow-2xl">
       <div className=" card-body text-8xl  font-extrabold">
         <span className=" pb-10 [&amp;::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&amp;::selection]:bg-blue-700/20 [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
           Happyjacks<br></br>Gallery !
