808d06976ecab4035d7d0ba79bc01131fab1cbfd
Sofia Bargues <barguessofia@gmail.com>
2024-03-02 12:28:16 +0000

    gdf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,42 +5,30 @@ import { navLinks } from "../constants/data";
 const Navbar = () => {
   return (
     <nav className="w-full flex py-1 justify-between items-center navbar  relative z-50 flex-0">
-      <div className="navbar bg-base-100">
-        <div className="drawer">
-          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content">
-            {/* Page content here */}
-            <label className="btn btn-circle swap swap-rotate">
-              {/* this hidden checkbox controls the state */}
-              <input type="checkbox" />
-
-              {/* hamburger icon */}
-              <svg
-                className="swap-off fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 512 512"
-              >
-                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-              </svg>
-
-              {/* close icon */}
-              <svg
-                className="swap-on fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 512 512"
-              >
-                <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-              </svg>
-            </label>
-          </div>
+      <a className="btn btn-ghost text-2xl text-slate-600 ">
+        Jack and Jill Windmills
+      </a>
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn m-1">
+          Menu
         </div>
-        <a className="btn btn-ghost text-2xl text-slate-600 ">
-          Jack and Jill Windmills
-        </a>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>History</a>
+          </li>
+          <li>
+            <a>Visits</a>
+          </li>
+          <li>
+            <a>Videos</a>
+          </li>
+          <li>
+            <a>Events</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
