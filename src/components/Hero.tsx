d887daad26b561eb4f9a1c9d35191ff7024b19dc
Sofia Bargues <barguessofia@gmail.com>
2024-03-02 08:50:44 +0000

    r

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,24 +2,28 @@
 
 const Hero = () => {
   return (
-    <section id="home">
+    <section id="home" className="relative">
       <div
-        className="hero min-h-screen"
+        className="hero"
         style={{
           backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+            "url(https://cdn.zenfolio.com/cdn2/pub/ntjenhvoufbc/0/null/m/kkzpjnokn_rpdop7uqsy/s/v-10/p1711332550-5.jpg?ts=ASH&tk=mrD3e3gkA5jeLopFEOHHImmMt5PHWXQ7XsqZqDmmxJo=&v=2&visitor=FwDtmueJtjbSRDYVTrhQQqxFwn5cAJS5ykBMewaBToLy&auth=exp=1709596799~acl=%2Fcdn2%2Fpub%2Fntjenhvoufbc%2F%2A~hmac=fa4e1f25b645245e93d6cffccba6d73a)",
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
+        <div className="hero-overlay bg-opacity-60 absolute top-0 left-0 right-0 bottom-0"></div>
+        <div className="hero-content py-80 text-right text-neutral-content  felx flex-col justify-around relative ">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <h1 className="mb-5 text-5xl font-bold">The Chattri</h1>
+            <p className="mb-28">
+              Is a memorial built to honour the Indian dead of the First World
+              War. It stands on the Downs near Patcham at the place where Hindu
+              and Sikh soldiers who died in Brighton war hospitals during
+              1914-1915 were cremated.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+          </div>
+          <div className="absolute bottom-5 text-right  left-0 right-0">
+            <p className="">Help the Chattri Memorial Group</p>
+            <button className="btn btn-xs btn-primary">Suport Us</button>
           </div>
         </div>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,121 @@ import { navLinks } from "../constants/data";
 
 const Navbar = () => {
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
+    <nav className="w-full flex py-1 justify-between items-center navbar  relative z-50">
       <div className="navbar bg-base-100">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <label
+              htmlFor="my-drawer"
+              className="btn btn-primary btn btn-square btn-ghost drawer-button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+              <div className="join join-vertical w-full">
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" defaultChecked />
+                  <div className="collapse-title text-xl font-medium">
+                    Chattri History
+                  </div>
+                  <div className="collapse-content">
+                    <li>
+                      <a>Brief History</a>
+                    </li>
+                    <li>
+                      <a>In Depth History</a>
+                    </li>
+                    <li>
+                      <a>Exhibition</a>
+                    </li>
+                  </div>
+                </div>
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" />
+                  <div className="collapse-title text-xl font-medium">
+                    Memorial Service & Events
+                  </div>
+                  <div className="collapse-content">
+                    <li>
+                      <a>Service</a>
+                    </li>
+                    <li>
+                      <a>Getting Here</a>
+                    </li>
+                    <li>
+                      <a>Other Events</a>
+                    </li>
+                    <li>
+                      <a>Royal Visit 2014</a>
+                    </li>
+                  </div>
+                </div>
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" />
+                  <div className="collapse-title text-xl font-medium">
+                    Photo Galleries
+                  </div>
+                  <div className="collapse-content">
+                    <li>
+                      <a>Service 2008</a>
+                    </li>
+                    <li>
+                      <a>Service 2012</a>
+                    </li>
+                    <li>
+                      <a>Service 2014</a>
+                    </li>
+                    <li>
+                      <a>Royal Visit 2014</a>
+                    </li>
+                  </div>
+                </div>
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" />
+                  <div className="collapse-title text-xl font-medium">
+                    Videos
+                  </div>
+                  <div className="collapse-content">
+                    <li>
+                      <a>Charttri Videos</a>
+                    </li>
+                  </div>
+                </div>
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                  <input type="radio" name="my-accordion-4" />
+                  <div className="collapse-title text-xl font-medium">Shop</div>
+                  <div className="collapse-content">
+                    <li>
+                      <a>Memorablia</a>
+                    </li>
+                  </div>
+                </div>
+              </div>
+            </ul>
+          </div>
+        </div>
+        <a className="btn btn-ghost text-2xl">Chattri</a>
       </div>
     </nav>
   );
