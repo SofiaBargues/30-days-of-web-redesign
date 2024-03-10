c1bc83a5672f3e56c13f521ea177a5d54cc06416
Sofia Bargues <barguessofia@gmail.com>
2024-03-10 06:30:11 +0000

    dfsf

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,73 +4,55 @@ import { Squares } from "./Scuares";
 
 export const Hero = () => {
   return (
-    <section id="home">
-      <div
-        className="hero h-full w-screen"
-        style={{
-          backgroundImage:
-            "url(https://www.totternhoememorialhall.com/uploads/7/1/3/6/71365849/whatsapp-image-2020-11-08-at-11-10-54-8.jpeg)",
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60 "></div>
-        <div className="flex flex-col hero-content pb-10 justify-end h-full text-center text-white">
-          <div className="max-w-2xl text-balance p-20 ">
-            <h1 className=" text-5xl pb-10 font-medium">
-              Your village-your Hall!
-            </h1>
-            <p className="mb-5 text-xl font-light">
-              The Memorial Hall was opened in 1928 and dedicated as a memorial
-              to those villagers who were killed in the First World War. Now a
-              days is maintained by trustees and run by a volunteer committee.
-            </p>
-            <div className="drawer drawer-end">
-              <input
-                id="my-drawer-4"
-                type="checkbox"
-                className="drawer-toggle"
-              />
-              <div className="drawer-content">
-                {/* Page content here */}
-                <label
-                  htmlFor="my-drawer-4"
-                  className="drawer-button btn btn-outline mt-10 font-extrabold"
-                >
-                  More Information
-                </label>
-              </div>
-              <div className="drawer-side">
-                <label
-                  htmlFor="my-drawer-4"
-                  aria-label="close sidebar"
-                  className="drawer-overlay"
-                ></label>
-                <ul className="menu py-40 gap-10  w-80 text-2xl  min-h-full bg-base-200 text-base-content">
-                  {/* Sidebar content here */}
-                  <li>
-                    <a>Home</a>
-                  </li>
-                  <li>
-                    <a>About</a>
-                  </li>
-                  <li>
-                    <a>Winter Beer Festival</a>
-                  </li>
-                  <li>
-                    <a>Remembrance</a>
-                  </li>
-                  <li>
-                    <a>The Bar</a>
-                  </li>
-                  <li>
-                    <a>Al Fresco</a>
-                  </li>
-                  <li>
-                    <a>Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+    <section className="bg-primary-content dark:bg-neutral-content">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+        <div className="flex flex-col justify-center">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            We invest in the world’s potential
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+            Here at Flowbite we focus on markets where technology, innovation,
+            and capital can unlock long-term value and drive economic growth.
+          </p>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
+            <a
+              href="#"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            >
+              Get started
+              <svg
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-neutral rounded-lg border border-neutral hover:bg-neutral-content hover:text-secondary focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              Learn more
+            </a>
           </div>
+        </div>
+        <div>
+          <iframe
+            className="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl"
+            src="https://www.youtube.com/embed/CJuQ5zUvWBU?si=hJFTEGxj8IZ6Ty8y&amp;start=9"
+            title="YouTube video player"
+            frameBorder={0}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen=""
+          />
         </div>
       </div>
     </section>
