13b58dbdbbc1e07e34c7cae9198dab12b201c92a
Sofia Bargues <barguessofia@gmail.com>
2024-03-15 18:19:37 +0000

    sdf

--- src/components/Proof.tsx
+++ src/components/Proof.tsx
@@ -2,62 +2,44 @@
 
 export const Proof = () => (
   <section>
-    <div className="h-400 py-10 px-6 bg-white">
-      <div className="grid grid-cols-6  gap-4">
-        <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-          <div className="relative group">
-            <img
-              className="w-full h-auto"
-              src="https://thedoghousepub.co.uk/wp-content/uploads/2024/01/trip-advisor-2023.jpg"
-              alt="Placeholder"
-            />
+    <div className="h-400 p-10  bg-neutral-50">
+      <div className="grid grid-cols-4  ">
+        <div className="relative group card w-32 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
-        </figure>
-        <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-          <div className="relative group">
-            <img
-              className="w-full h-auto"
-              src="https://thedoghousepub.co.uk/wp-content/uploads/2022/06/lux.png"
-              alt="Placeholder"
-            />
+        </div>
+        <div className="relative group card w-32 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
-        </figure>
-        <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-          <div className="relative group">
-            <img
-              className="w-full h-auto"
-              src="https://thedoghousepub.co.uk/wp-content/uploads/2023/04/sluurpy-199x300.jpg"
-              alt="Placeholder"
-            />
+        </div>
+        <div className=" relative group card w-32 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
-        </figure>
-        <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-          <div className="relative group">
-            <img
-              className="w-full h-auto"
-              src="https://thedoghousepub.co.uk/wp-content/uploads/2023/04/global.jpg"
-              alt="Placeholder"
-            />
+        </div>
+        <div className="card w-32 relative group bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
-        </figure>
-        <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-          <div className="relative group">
-            <img
-              className="w-full h-auto"
-              src="https://thedoghousepub.co.uk/wp-content/uploads/2024/01/good-beer.jpg"
-              alt="Placeholder"
-            />
-          </div>
-        </figure>
-        <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-          <div className="relative group">
-            <img
-              className="w-full h-auto"
-              src="https://thedoghousepub.co.uk/wp-content/uploads/2024/01/dog-friendly.jpg"
-              alt="Placeholder"
-            />
-          </div>
-        </figure>
+        </div>
       </div>
     </div>
   </section>
--- src/components/Visit.tsx
+++ src/components/Visit.tsx
@@ -7,8 +7,7 @@ export const Visit = () => (
   >
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26009.417991541537!2d-4.41811617638055!3d53.00756832416523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4865bc557f3f4ea1%3A0xff433a6df8a64be9!2sAberafon%20Camping%20and%20Caravan%20Site!5e0!3m2!1sen!2suk!4v1710524120307!5m2!1sen!2suk"
-      width={600}
-      height={450}
+      className="w-full h-96"
       style={{ border: 0 }}
       allowFullScreen=""
       loading="lazy"
