0643ab1b8159fefe65fafbf204c518b5ae2f5fe5
Sofia Bargues <barguessofia@gmail.com>
2024-03-17 01:18:25 +0000

    ytr

--- src/components/Visit.tsx
+++ src/components/Visit.tsx
@@ -7,8 +7,7 @@ export const Visit = () => (
   >
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1230.4903206248887!2d-0.61714505!3d51.916064299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876519f30f458e3%3A0xf475c3a92470fb58!2stigi%20-%20tabu%20cattery!5e0!3m2!1sen!2suk!4v1710674528943!5m2!1sen!2suk"
-      width={600}
-      height={450}
+      className="w-full h-96"
       style={{ border: 0 }}
       allowFullScreen=""
       loading="lazy"
