b64c5cd9f15b9d3f1826a41d69ba6e428278abb9
Sofia Bargues <barguessofia@gmail.com>
2024-03-17 01:17:23 +0000

    rges

--- src/components/Visit.tsx
+++ src/components/Visit.tsx
@@ -6,8 +6,9 @@ export const Visit = () => (
   "
   >
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26009.417991541537!2d-4.41811617638055!3d53.00756832416523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4865bc557f3f4ea1%3A0xff433a6df8a64be9!2sAberafon%20Camping%20and%20Caravan%20Site!5e0!3m2!1sen!2suk!4v1710524120307!5m2!1sen!2suk"
-      className="w-full h-96"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1230.4903206248887!2d-0.61714505!3d51.916064299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876519f30f458e3%3A0xf475c3a92470fb58!2stigi%20-%20tabu%20cattery!5e0!3m2!1sen!2suk!4v1710674528943!5m2!1sen!2suk"
+      width={600}
+      height={450}
       style={{ border: 0 }}
       allowFullScreen=""
       loading="lazy"
