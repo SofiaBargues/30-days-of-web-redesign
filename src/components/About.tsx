df0a283eaab6359210b0165c65f583b195c15790
Sofia Bargues <barguessofia@gmail.com>
2024-03-18 20:40:40 +0000

    safd

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,11 @@
 // import { logo } from "../assets";
 
 export const About = () => (
-  <div className="overflow-x-auto m-20 mb-32 ">
-    <h1 className="text-2xl font-serif font-semibold text-center m-10">
-      History
-    </h1>
+  <div className="overflow-x-auto p-20 ">
+    <h1 className="text-4xl text-right font-bold mx-10">About Us</h1>
 
     <div className="flex flex-row-reverse m-20 gap-2">
-      <p className="text-md text-start text-balance">
+      <p className="text-md text-start text-balance w-[1000px]">
         A modern, spacious and multi-functional venue for parties, classes,
         activity groups, business meetings, presentations, social and corporate
         events. This modern hall was opened in 2014, constructed of beautiful
@@ -19,7 +17,7 @@ export const About = () => (
       <div className="bg-neutral h-50 w-[6px]"></div>
     </div>
     <div className="flex flex-row m-20 gap-2">
-      <p className="text-md text-end text-balance">
+      <p className="text-md text-end text-balance  w-[1000px]">
         ​For general enquiries please call on 01296 532393, ​or e-mail us at
         hall@ovingpc.com. For bookings please click here. Online booking is now
         available. ​ Information about hire charges can be found by clicking
