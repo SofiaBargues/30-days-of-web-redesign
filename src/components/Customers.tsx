4adcd66c72fa4db2541e21f167eb204676947634
Sofia Bargues <barguessofia@gmail.com>
2024-03-18 19:40:53 +0000

    fsd

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -3,7 +3,19 @@
 export const Customers = () => (
   <div className="grid grid-cols-3  text-right">
     <div className="flex m-10">
-      <h1 className="text-md font-serif font-semibold text-center ">History</h1>
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
+        <div className="w-6 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+          />
+        </div>
+      </div>
+      <h1 className="text-md font-serif font-semibold m-3">History</h1>
       <div className="rating rating-sm flex flex-row px-10 items-center ">
         <input
           type="radio"
@@ -29,7 +41,19 @@ export const Customers = () => (
       </div>
     </div>
     <div className="flex m-10">
-      <h1 className="text-md font-serif font-semibold text-center ">History</h1>
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
+        <div className="w-6 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+          />
+        </div>
+      </div>
+      <h1 className="text-md font-serif font-semibold m-3">History</h1>
       <div className="rating rating-sm flex flex-row px-10 items-center ">
         <input
           type="radio"
@@ -51,15 +75,23 @@ export const Customers = () => (
           name="rating-6"
           className="mask mask-star-2 bg-orange-400"
         />
-        <input
-          type="radio"
-          name="rating-6"
-          className="mask mask-star-2 bg-orange-400"
-        />
+        <input type="radio" name="rating-6" className="mask mask-star-2" />
       </div>
     </div>
     <div className="flex m-10">
-      <h1 className="text-md font-serif font-semibold text-center ">History</h1>
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
+        <div className="w-6 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+          />
+        </div>
+      </div>
+      <h1 className="text-md font-serif font-semibold m-3">History</h1>
       <div className="rating rating-sm flex flex-row px-10 items-center ">
         <input
           type="radio"
