739a9b8a88fc1cc15a75df7693f7153d2a2bb82a
Sofia Bargues <barguessofia@gmail.com>
2024-03-18 12:33:16 +0000

    vbxcv

--- src/components/Visit.tsx
+++ src/components/Visit.tsx
@@ -1,13 +1,10 @@
 // import { logo } from "../assets";
 
 export const Visit = () => (
-  <div
-    className="
-  "
-  >
+  <div className=" m-9">
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4926.270244991482!2d-0.823147!3d51.876746!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876f9ba1925d2c7%3A0xc3bdeb833656f2fa!2sParrott%20Brothers!5e0!3m2!1sen!2sus!4v1710870863200!5m2!1sen!2sus"
-      className="w-full h-96"
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d60458.7346706478!2d-0.933878!3d51.869833!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876faf6a591575b%3A0x792c414ba584c2a9!2sQuainton%20Windmill!5e1!3m2!1sen!2sus!4v1710959018090!5m2!1sen!2sus"
+      className="w-full h-96 rounded-3xl"
       style={{ border: 0 }}
       allowFullScreen=""
       loading="lazy"
