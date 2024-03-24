a8c96e502932d245fce11c4bdc238c3bc68cb6c6
Sofia Bargues <barguessofia@gmail.com>
2024-03-24 14:51:47 +0000

    dfgdf

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -4,9 +4,13 @@ export const Music = () => (
   <>
     <div className="hero  bg-base-200">
       <div className="hero-content flex-col sm:flex-row">
-        <img
-          src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-          className="max-w-sm rounded-lg shadow-2xl"
+        <iframe
+          src="https://open.spotify.com/embed/playlist/4RySonVVEMgGilMarxe7w1?wmode=opaque"
+          className="w-full h-96  rounded-3xl"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
         />
         <div>
           <h1 className="text-5xl font-bold">Box Office News!</h1>
@@ -16,6 +20,25 @@ export const Music = () => (
             a id nisi.
           </p>
           <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+
+    <div className=" p-20">
+      <h1 className="text-4xl text-right font-bold mx-10">Where find us?</h1>
+      <div className="">
+        <div className="bg-white  mt-10 border rounded-2xl border-base-300 m-5">
+          <div className=" p-8 flex flex-col gap-10 shadow-2xl rounded-2xl">
+            <div className="mockup-window border border-base-300 m-20m-20 flex justify-center border-t"></div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2502.955464319433!2d0.9250114765984924!3d51.14617237173352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dedace43b02073%3A0xcd8472beeb922af!2sSt%20Mary's%20Church%2C%20Hinxhill!5e0!3m2!1sen!2suk!4v1711016168596!5m2!1sen!2suk"
+              className="w-full h-96  rounded-3xl"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
         </div>
       </div>
     </div>
