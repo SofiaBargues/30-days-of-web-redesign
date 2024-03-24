116fa5645b13eff4bfa8ce46646b44ee4cd2e313
Sofia Bargues <barguessofia@gmail.com>
2024-03-24 16:07:09 +0000

    sdd

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,7 +7,6 @@ export const Map = () => {
       <div className="">
         <div className="bg-white  mt-10 border rounded-2xl border-base-300 m-5">
           <div className=" p-8 flex flex-col gap-10 shadow-2xl rounded-2xl">
-            <div className="mockup-window border border-base-300 m-20m-20 flex justify-center border-t"></div>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4976.1285958591825!2d-0.0674621!3d51.4202455!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487601675f83a341%3A0xbbe9342ed722fb88!2sCrystal%20Palace%20Bowl!5e0!3m2!1sen!2suk!4v1711295984034!5m2!1sen!2suk"
               className="w-full h-96  rounded-3xl"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Squares } from "./Scuares";
 
 export const Navbar = () => {
   return (
-    <nav className="bg-transparent  dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-transparent  dark:bg-gray-900 fixed w-full z-20 top-0 start-0">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <img
           src="https://images.squarespace-cdn.com/content/v1/5e53fd130b5d7822b0c250a5/1613264173858-RB7H4N7U0WG6GMF3RYXY/Cutout+Logo.png?format=1500w"
