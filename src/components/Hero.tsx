0005ed1fc2986a12608f200a3f37fa9adfb0c0bc
Sofia Bargues <barguessofia@gmail.com>
2024-03-28 11:03:40 +0000

    gdf

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,35 +7,34 @@ import { Cards } from "./Card";
 
 export const Hero = () => {
   return (
-    <div
-      className="hero"
-      style={{
-        backgroundImage:
-          "url(https://scontent-lhr6-2.xx.fbcdn.net/v/t39.30808-6/295347821_932703234779813_3682614714142714646_n.jpg?stp=dst-jpg_s960x960&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=5ICRe13CZ10AX823T9u&_nc_ht=scontent-lhr6-2.xx&oh=00_AfCOsR0Dzs6WADnc6n8LLcSk_C4BFpFn5V0v4rIr4ysh5w&oe=660A2A89)",
-      }}
-    >
-      <div className="hero-overlay bg-opacity-40"></div>
-      <div className="hero-content text-neutral-content">
-        <div className="flex flex-col m-w-screen h-screen justify-around   ">
-          <div>
-            <h1 className=" text-white text-7xl mt-60 font-bold flex justify-center  text-balance">
-              Crystal Palace Bowl
-            </h1>
-            <div className="flex flex-col justify-center m-20">
-              <p className=" text-2xl  text-neutral-400 text-balance text-center ">
-                Crystal Palace Bowl is an iconic South London venue, with award
-                winning architecture and over 60 years of live music heritage.
-              </p>
-            </div>
-            <div className="flex flex-col justify-center mx-96">
-              <button className="btn btn-lg text-white btn-primary ">
-                Crowdfunding
-              </button>
-            </div>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />{" "}
+        <label className="swap swap-flip text-9xl">
+          {/* this hidden checkbox controls the state */}
+          <input type="checkbox" />
+
+          <div className="swap-on">
+            <img
+              src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            😈
           </div>
-          <h1 className=" font-medium text-xl mx-10 flex justify-end  text-slate-300  ">
-            SUPORTED BY MAYOR OF LONDON
-          </h1>
+          <div className="swap-off">😇</div>
+        </label>
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6 max-w-[600px]">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+
+          <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
     </div>
