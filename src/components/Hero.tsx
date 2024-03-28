3de3809dc8fd67f0e812a1ca168d6e7cf115ab2b
Sofia Bargues <barguessofia@gmail.com>
2024-03-28 15:18:55 +0000

    dfg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,33 +8,26 @@ import { Cards } from "./Card";
 export const Hero = () => {
   return (
     <div className=" bg-[#ffd500] ">
-      <div className="flex flex-row-2">
-        <label className="swap swap-flip text-9xl">
-          <input type="checkbox" className="hidden" />
-
-          <div className="swap-on ">
-            <div className="grid grid-cols-2">
-              <div className="w-[1000px]">
+      <div className="flex items-center">
+        <div className="swap swap-flip text-9xl">
+          <label className="swap">
+            <input type="checkbox" className="hidden" />
+            <div className="swap-on">
+              <div className=" w-40 h-40">
                 <img
                   src="https://static.wixstatic.com/media/29f523_45e7c7b98b134fbca70d49caf42a1bd1~mv2.png/v1/fill/w_600,h_380,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/29f523_45e7c7b98b134fbca70d49caf42a1bd1~mv2.png"
                   className=""
                 />
               </div>
             </div>
-          </div>
-          <div className="swap-off ">
-            <div className=" items-center">
-              <img
-                src="https://static.wixstatic.com/media/29f523_17e3bbe87f0f4aee976911f8de492327~mv2.png/v1/fill/w_110,h_111,al_c,q_85,enc_auto/29f523_17e3bbe87f0f4aee976911f8de492327~mv2.png"
-                className=""
-              />
+            <div className="swap-off">
               <img
                 src="https://static.wixstatic.com/media/59d2f4_f086fff0f5c24080bb07f6569b5b7a26~mv2.png/v1/fill/w_453,h_352,al_c,lg_1,q_85,enc_auto/guayma-de-oro.png"
-                className=""
+                className="w-40 h-40"
               />
             </div>
-          </div>
-        </label>
+          </label>
+        </div>
         <div>
           <h1 className="text-5xl font-bold">Box Office News!</h1>
           <p className="py-6 max-w-[600px]">
